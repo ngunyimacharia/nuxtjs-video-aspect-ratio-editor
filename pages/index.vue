c0e3194db277138c006ee2a94546983f601b1d41
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-10-15 03:05:29 +0200

    form added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,82 @@
 <template>
-  <div></div>
-</template>
+  <div
+    class="
+      min-h-screen
+      bg-gray-50
+      flex flex-col
+      justify-center
+      py-12
+      sm:px-6
+      lg:px-8
+    "
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Video aspect ratio editor
+      </h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Upload your video to have it rendered in multiple aspect ratios
+      </p>
+    </div>
 
-<script>
-export default {};
-</script>
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" action="#" method="POST">
+          <div>
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              Video
+            </label>
+            <div class="mt-1">
+              <input
+                id="file"
+                name="file"
+                type="file"
+                required
+                class="
+                  appearance-none
+                  block
+                  w-full
+                  px-3
+                  py-2
+                  border border-gray-300
+                  rounded-md
+                  shadow-sm
+                  placeholder-gray-400
+                  focus:outline-none
+                  focus:ring-indigo-500
+                  focus:border-indigo-500
+                  sm:text-sm
+                "
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="
+                w-full
+                flex
+                justify-center
+                py-2
+                px-4
+                border border-transparent
+                rounded-md
+                shadow-sm
+                text-sm
+                font-medium
+                text-white
+                bg-indigo-600
+                hover:bg-indigo-700
+                focus:outline-none
+                focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+              "
+            >
+              Upload
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
